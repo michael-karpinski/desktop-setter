b1aa8973de4e08b97d6038374a51d457f7943bca
Michael Karpinski <mike.karpinski99@gmail.com>
2020-01-02 23:55:00 -0500

    fixed error in determining file extension

--- back_end/main.py
+++ back_end/main.py
@@ -9,6 +9,8 @@ from PyQt5 import QtWidgets
 import back_end.img_functions as img_functions
 from back_end import wallpaper
 
+FILE_EXTENSIONS = ['jpg', 'png']
+
 
 def run():
     """Determines platform, finds and downloads appropriate image, and sets desktop background."""
@@ -37,8 +39,13 @@ def get_file_path(url):
             file_obj.write(folder)
     file_name = folder + os.path.sep + \
         datetime.datetime.now().strftime('%d-%b-%Y_%H:%M:%S') + '.'
-    try:
-        file_name += url.split('.')[-1][:4]
-    except Exception:
-        file_name += 'png'
+    file_name += get_extension(url)
     return file_name
+
+
+def get_extension(url):
+    """Determines file type based on URL. Defaults to PNG."""
+    for ext in FILE_EXTENSIONS:
+        if ext in url:
+            return ext
+    return 'png'
